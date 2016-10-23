cdaf109b5bb0e4059c5636dc01a71f66ee070e16
soulmirago <james.m.dalton@gmail.com>
2016-10-22 22:28:19 -0400

    Update bot.go

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -1,1 +1,184 @@
-test
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"os/signal"
+	"runtime"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/bwmarrin/discordgo"
+	"github.com/dustin/go-humanize"
+)
+
+var (
+	// discordgo session
+	discord *discordgo.Session
+
+	// Redis client connection (used for stats)
+	//rcli *redis.Client
+
+	// Map of Guild id's to *Play channels, used for queuing and rate-limiting guilds
+	//queues map[string]chan *Play = make(map[string]chan *Play)
+
+	// Sound encoding settings
+	BITRATE        = 128
+	MAX_QUEUE_SIZE = 6
+
+	// Owner
+	OWNER string
+)
+
+func onReady(s *discordgo.Session, event *discordgo.Ready) {
+	log.Info("Recieved READY payload")
+	//s.UpdateStatus(0, "airhornbot.com")
+}
+
+func onGuildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+	if event.Guild.Unavailable != nil {
+		return
+	}
+
+	for _, channel := range event.Guild.Channels {
+		if channel.ID == event.Guild.ID {
+			s.ChannelMessageSend(channel.ID, "**Nowbot ready.**")
+			return
+		}
+	}
+}
+
+func scontains(key string, options ...string) bool {
+	for _, item := range options {
+		if item == key {
+			return true
+		}
+	}
+	return false
+}
+func utilGetMentioned(s *discordgo.Session, m *discordgo.MessageCreate) *discordgo.User {
+	for _, mention := range m.Mentions {
+		if mention.ID != s.State.Ready.User.ID {
+			return mention
+		}
+	}
+	return nil
+}
+
+// Handles bot operator messages, should be refactored (lmao)
+func handleBotControlMessages(s *discordgo.Session, m *discordgo.MessageCreate, parts []string, g *discordgo.Guild) {
+	if scontains(parts[1], "!nowbot") {
+		s.ChannelMessageSend(m.ChannelID, "Testing"
+  }
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if len(m.Content) <= 0 || (m.Content[0] != '!' && len(m.Mentions) < 1) {
+		return
+	}
+
+	msg := strings.Replace(m.ContentWithMentionsReplaced(), s.State.Ready.User.Username, "username", 1)
+	parts := strings.Split(strings.ToLower(msg), " ")
+
+	channel, _ := discord.State.Channel(m.ChannelID)
+	if channel == nil {
+		log.WithFields(log.Fields{
+			"channel": m.ChannelID,
+			"message": m.ID,
+		}).Warning("Failed to grab channel")
+		return
+	}
+
+	guild, _ := discord.State.Guild(channel.GuildID)
+	if guild == nil {
+		log.WithFields(log.Fields{
+			"guild":   channel.GuildID,
+			"channel": channel,
+			"message": m.ID,
+		}).Warning("Failed to grab guild")
+		return
+	}
+
+	// If this is a mention, it should come from the owner (otherwise we don't care)
+	if len(m.Mentions) > 0 && m.Author.ID == OWNER && len(parts) > 0 {
+		mentioned := false
+		for _, mention := range m.Mentions {
+			mentioned = (mention.ID == s.State.Ready.User.ID)
+			if mentioned {
+				break
+			}
+		}
+
+		if mentioned {
+			handleBotControlMessages(s, m, parts, guild)
+		}
+		return
+	}
+}
+
+func main() {
+	var (
+		Token      = flag.String("t", "", "Discord Authentication Token")
+		Shard      = flag.String("s", "", "Shard ID")
+		ShardCount = flag.String("c", "", "Number of shards")
+		Owner      = flag.String("o", "", "Owner ID")
+		err        error
+	)
+	flag.Parse()
+
+	if *Owner != "" {
+		OWNER = *Owner
+	}
+
+	// Preload
+	//log.Info("Preloading sounds...")
+	//for _, coll := range COLLECTIONS {
+	//	coll.Load()
+	//}
+	
+	// Create a discord session
+	log.Info("Starting discord session...")
+	discord, err = discordgo.New(*Token)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Failed to create discord session")
+		return
+	}
+
+	// Set sharding info
+	discord.ShardID, _ = strconv.Atoi(*Shard)
+	discord.ShardCount, _ = strconv.Atoi(*ShardCount)
+
+	if discord.ShardCount <= 0 {
+		discord.ShardCount = 1
+	}
+
+	discord.AddHandler(onReady)
+	discord.AddHandler(onGuildCreate)
+	discord.AddHandler(onMessageCreate)
+
+	err = discord.Open()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Failed to create discord websocket connection")
+		return
+	}
+
+	// We're running!
+	log.Info("Nowbot ready.")
+
+	// Wait for a signal to quit
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	<-c
+}
